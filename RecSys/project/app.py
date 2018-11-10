c296b9d9007bca2421027481c120f06005491394
Maksim <maxi.petrov97@mail.ru>
2018-11-10 21:47:15 +0300

    added request to wikidata

--- RecSys/project/app.py
+++ RecSys/project/app.py
@@ -2,6 +2,7 @@ import json
 
 from RecSys.project.sugrate import UserSuggestingRate
 from RecSys.project.util import parseCsv
+from RecSys.project.util import getDirectorAwardsFromFilmName
 
 rateFile = "../data/data.csv"
 weeklyFile = "../data/context_day.csv"
@@ -11,12 +12,27 @@ usersRates = parseCsv(rateFile, int)
 usersDaysOfWeek = parseCsv(weeklyFile, str)
 usersPlaces = parseCsv(placeFile, str)
 
-myName = "User 1"
+myName = "User 5"
 userSuggestion = UserSuggestingRate(myName=myName, usersRates=usersRates, usersDaysOfWeek=usersDaysOfWeek, usersPlaces=usersPlaces)
+filmIndexRec = userSuggestion.recommend()
+filmNumberRec = "Movie " + str(filmIndexRec+1)
+
+
+filmNames = parseCsv(fileName="../data/Movie_names.csv", valueType=str, skipFirstLine=False)
+
+filmNameRec = ""
+directorInfo = ""
+if filmNumberRec in filmNames.keys():
+    filmNameRec = filmNames[filmNumberRec][0].strip()
+    directorInfo = getDirectorAwardsFromFilmName(filmNameRec)
+
 result = {
     "User": myName,
     "Rates forecast": userSuggestion.suggestRates(),
-    "Film recommendation": "Movie " + str(userSuggestion.recommend())
+    "Film recommendation": filmNumberRec,
+    "Film name": filmNameRec,
+    "Director name": directorInfo["name"],
+    "Director awards": directorInfo["awards"]
 }
 with open('../'+myName+'.json', 'w') as outfile:
     json.dump(obj=result, fp=outfile, indent=4)
--- RecSys/project/util.py
+++ RecSys/project/util.py
@@ -1,12 +1,40 @@
 import csv
+from SPARQLWrapper import SPARQLWrapper, JSON
 
-def parseCsv(fileName: str, valueType: type) -> dict:
+def getDirectorAwardsFromFilmName(filmName: str) -> dict:
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+    sparql.setQuery("""SELECT ?directorLabel ?awardLabel
+    WHERE {
+      ?film wdt:P31 wd:Q11424.
+      ?film wdt:P1476 ?title.
+      """ + f"""FILTER regex(?title, "^{filmName}"). """ + """
+      ?film wdt:P57 ?director.
+      ?director wdt:P166 ?award.
+
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }
+    """)
+    sparql.setReturnFormat(JSON)
+    queryResults = sparql.query().convert()["results"]["bindings"]
+
+    results = dict()
+    results["name"] = queryResults[0]["directorLabel"]["value"]
+    results["awards"] = list()
+
+    for award in queryResults:
+        results["awards"].append(award["awardLabel"]["value"])
+
+    return results
+
+def parseCsv(fileName: str, valueType: type, skipFirstLine: bool = True ) -> dict:
+    result = dict()
+
     with open(fileName, newline='') as csvfile:
         reader = csv.reader(csvfile)
-        next(reader)
+        if (skipFirstLine):
+            next(reader)
         fileLines = list(reader)
 
-    result = dict()
     for line in fileLines:
         values = list()
         for value in line[1:len(line)]:
